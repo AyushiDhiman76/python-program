d626a45acdef4f5b681fbc38d639ec7c034c91cb
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-02-25 12:56:55 +0530

    Add files via upload

---
+++ a XII.py
@@ -0,0 +1,7 @@
+# WAP to print all the names from list whose length is greater then 6.
+names = ['Ayushi', 'Lokesh', 'Anuradha', 'Harshit', 'Divyansh', 'Deepak', 'Nishita', 'Tushar', 'Anurag', 'Mouli', 'Vikash']
+
+# Print all names whose length is greater than 6
+for name in names:
+    if len(name) > 6:
+        print(name)
