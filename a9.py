a1f4354d741e9d76e79b1906a9e86babb348091b
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-02-25 12:18:16 +0530

    Add files via upload

---
+++ a9.py
@@ -0,0 +1,11 @@
+#WAP to display the first occurance of number divisible by kin the list
+def first_divisible_by_k(numbers, k):
+    for num in numbers:
+        if num % k == 0:
+            return num
+numbers= [4, 7, 2, 9, 6, 1, 3, 8]
+k=3
+
+result = first_divisible_by_k(numbers, k)
+print(result)     
+    
