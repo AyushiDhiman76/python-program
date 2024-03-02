dcb89c39a130ab1829e2adb3fba1764dd6ac6e73
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-03-02 15:07:10 +0530

    Add files via upload

--- Check a particular substring present in string.py
+++ Check a particular substring present in string.py
@@ -1,12 +1,13 @@
-#WAP to check a particular substring present in the string.
-def is_substring_present(string, substring):
-    if substring in string:
-        return True
-    else:
-        return False
+#WAP to find nth largest and nth smallest elemenrt from list.
+def nth_largest(numbers, n):
+    numbers.sort(reverse=True)
+    return numbers[n-1]
 
-string = "Hello, welcome to Python programming!"
-substring = "Python"
-print(is_substring_present(string, substring))
-substring = "Java"
-print(is_substring_present(string, substring))  
+def nth_smallest(numbers, n):
+    numbers.sort()
+    return numbers[n-1]
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+n = 3
+print("Nth largest element:", nth_largest(numbers, n))  
+print("Nth smallest element:", nth_smallest(numbers, n))  
