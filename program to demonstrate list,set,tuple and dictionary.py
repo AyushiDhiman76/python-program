ba34945ed724d3d156defab6d73d1a1036a5d542
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-03-02 15:14:59 +0530

    Add files via upload

---
+++ program to demonstrate list,set,tuple and dictionary.py
@@ -0,0 +1,12 @@
+#WAP to demonstrate list,set,tuple and dictionary
+l=[2,4,6,9]
+print("list :",l)
+
+s={2,1,4,6,7}
+print("set :",s)
+
+t=(4,5,2,8,7)
+print("tuple :",t)
+
+d={1:"1",2:"2",3:"3"}
+print("dictionary :",d)
